0a0ee72b84cb0eafa6ff78105f9058923606f428
Jay Bhakhar <jaybhakhar@gmail.com>
2021-08-02 03:25:34 +0500

    upload file by moblie app, succeed in Unit Test

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,12 @@
 import 'package:advance_video_share/unit_test.dart';
 import 'package:advance_video_share/views/pages/home_page.dart';
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
 
-void main() {
+
+void main() async{
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(MyApp());
 }
 
