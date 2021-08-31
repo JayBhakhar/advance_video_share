549980f5aa622039760f6ec36f9218a0e11a0ab5
Jay Bhakhar <jaybhakhar@gmail.com>
2021-09-01 01:37:07 +0500

    in work download button error

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,11 @@
 import 'package:advance_video_share/unit_test.dart';
 import 'package:advance_video_share/views/pages/splash_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_downloader/flutter_downloader.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await FlutterDownloader.initialize();
   runApp(MyApp());
 }
 
--- lib/views/pages/play_video_landscape.dart
+++ lib/views/pages/play_video_landscape.dart
@@ -1,5 +1,10 @@
+import 'dart:isolate';
+import 'dart:ui';
+import 'package:permission_handler/permission_handler.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_downloader/flutter_downloader.dart';
 import 'package:video_player/video_player.dart';
+import 'package:path_provider/path_provider.dart';
 
 class PlayVideoLandscape extends StatefulWidget {
   final String videoUrl;
@@ -11,17 +16,50 @@ class PlayVideoLandscape extends StatefulWidget {
 
 class _PlayVideoLandscapeState extends State<PlayVideoLandscape> {
   VideoPlayerController _controller;
+  int progress = 0;
+  ReceivePort receivePort = ReceivePort();
 
   @override
   void initState() {
     super.initState();
+    IsolateNameServer.registerPortWithName(
+        receivePort.sendPort, "downloadingvideo");
+
+    receivePort.listen((message) {
+      setState(() {
+        progress = message;
+      });
+    });
+
+    FlutterDownloader.registerCallback(downloadCallback);
+
     _controller = VideoPlayerController.network(widget.videoUrl);
     _controller.addListener(() {
       setState(() {});
     });
+
     _controller.setLooping(true);
     _controller.initialize().then((_) => setState(() {}));
     _controller.play();
+  }
+
+  static downloadCallback(id, status, progess) {
+    SendPort sendPort = IsolateNameServer.lookupPortByName('downloadingvideo');
+    sendPort.send(progess);
+  }
+
+  void _downloadFile() async {
+    final status = await Permission.storage.request();
+    if (status.isGranted) {
+      final baseStorage = await getDownloadsDirectory();
+      print('+++++++++++++++++++++++++++++++++ ${baseStorage.path}');
+      final id = await FlutterDownloader.enqueue(
+          url: widget.videoUrl,
+          savedDir: baseStorage.path,
+          fileName: 'firstfile2.mp4');
+    } else {
+      print('No permission');
+    }
   }
 
   @override
@@ -37,6 +75,38 @@ class _PlayVideoLandscapeState extends State<PlayVideoLandscape> {
             _controller,
             // VideoPlayerController.network(widget.videoUrl)..initialize(),
           ),
+        ),
+        floatingActionButton: Column(
+          mainAxisAlignment: MainAxisAlignment.end,
+          children: [
+            FloatingActionButton(
+              onPressed: () {
+                _downloadFile();
+              },
+              child: Icon(Icons.download_done_rounded),
+            ),
+            SizedBox(
+              height: 5,
+            ),
+            FloatingActionButton(
+              onPressed: () {},
+              child: Icon(Icons.ac_unit_outlined),
+            ),
+            SizedBox(
+              height: 5,
+            ),
+            FloatingActionButton(
+              onPressed: () {},
+              child: Icon(Icons.access_alarm),
+            ),
+            SizedBox(
+              height: 5,
+            ),
+            FloatingActionButton(
+              onPressed: () {},
+              child: Icon(Icons.accessible_forward_rounded),
+            ),
+          ],
         ),
       ),
     );
